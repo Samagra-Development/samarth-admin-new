4ffcf9076e2af736d0cf7cddf7e3b60fcc5548d1
Demon <him.sharma.98@gmail.com>
2022-05-14 11:04:02 +0530

    Migration in progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type {NextPage} from 'next'
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 const Home: NextPage = () => {
     const router = useRouter();
-    router.push('/login');
+    useEffect(() => {
+        router.push('/login');
+    }, [])
     return (
         <div>
         </div>
